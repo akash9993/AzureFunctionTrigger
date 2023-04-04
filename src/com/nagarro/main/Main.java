284b2509694bc7bbabce0a926fe480ad4ccc24c6
Akash <akash03@nagarro.com>
2023-04-04 11:01:00 +0530

    first commit

---
+++ src/com/nagarro/main/Main.java
@@ -0,0 +1,12 @@
+package com.nagarro.main;
+
+public class Main {
+
+	public static void functionTriggeredAfterFileUpload() {
+		System.out.println("file upload");
+	}
+
+	public static void main(String[] args) {
+		functionTriggeredAfterFileUpload();
+	}
+}
